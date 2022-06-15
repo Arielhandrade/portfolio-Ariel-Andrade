c269a93114712c4b572ffa8f33d6fe54294d4f3d
Arielhandrade <arielhandrade@gmail.com>
2022-06-15 20:15:47 -0300

    archivo cv modificado para descargar

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+/* ENRUTADORES */
 var indexRouter = require('./routes/index');
 var adminRouter = require('./routes/admin');
 var sectionsRouter = require('./routes/sections');
@@ -14,12 +15,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+/* MIDDLEWARES NIVEL APLICACION */
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static('public'));
 
+/* RUTAS */
 app.use('/', indexRouter);
 app.use('/administrador', adminRouter);
 app.use('/portfolio', sectionsRouter);
